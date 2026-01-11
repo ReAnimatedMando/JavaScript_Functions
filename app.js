1ee79ebbb8224c93929d14b0b61c91cfd02ada29
Aaron Tanguma <the_guma@yahoo.com>
2026-01-11 11:14:01 -0700

    completed challenge 1 in JS Functions

--- app.js
+++ app.js
@@ -3,5 +3,31 @@ console.log("Hello World!\n==========\n");
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
 
+function printOdds(count) {
+
+    if (count <= 0){
+        console.log("Invalid number");
+    }
+
+    for (let i = 1; i <= count; i++){
+        if(i % 2 != 0) {
+            console.log(i);
+        }
+    }
+}
+
+printOdds(16);
+printOdds(100);
+printOdds(-6);
+
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
+
+// Exercise 3 Section
+console.log("EXERCISE 3:\n==========\n");
+
+// Exercise 4 Section
+console.log("EXERCISE 4:\n==========\n");
+
+// Exercise 5 Section
+console.log("EXERCISE 5:\n==========\n");
