5ea80fb778c0cab9434c426485ce631e9652f7b0
Aaron Tanguma <the_guma@yahoo.com>
2026-01-11 17:26:59 -0700

    completed exercise 4 of JS Functions

--- app.js
+++ app.js
@@ -75,7 +75,29 @@ whichQuadrant(6, -2);
 // Exercise 4 Section
 console.log("EXERCISE 4:\n==========\n");
 
+function isTriangle(a, b, c) {
+    return a + b > c && a + c > b && b + c > a;
+}
 
+function triangleType (a, b, c) {
+    let triangle = isTriangle(a, b, c);
+    if (triangle) {
+        if (a == b && b == c) {
+            return 'Equilateral';
+        } else if (a == b || b == c || a == c) {
+            return 'Isosceles';
+        } else {
+            return 'Scalene';
+        }
+    } else {
+        return 'Not a valid triangle!';
+    }
+}
+
+console.log(triangleType(3, 3, 3));
+console.log(triangleType(3, 4, 2));
+console.log(triangleType(4, 4, 2));
+console.log(triangleType(1, 1, 2));
 
 // Exercise 5 Section
 console.log("EXERCISE 5:\n==========\n");
